dccf4a7cc09c72b2959a7019f3e82a8170049c04
Bruna Santos <brunasan7os@gmail.com>
2025-04-07 21:53:08 -0300

    Exercicio 08

---
+++ ListaDeExercicios.Exercicio08/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListaDeExercicios.Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+            #region input de dados
+            Console.WriteLine("Digite o valor de A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de C: ");
+            int C = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region processamento e output
+            if (A + B > C)
+            {
+                Console.WriteLine(" A soma de A + B não é menor que C");
+            }
+            else
+                Console.WriteLine("A soma de A + B é menor que C");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
