8cc70aeb2de0d00819e69f593f098c1e32198f3f
Bruna Santos <brunasan7os@gmail.com>
2025-04-07 23:28:51 -0300

    Exercício 15

---
+++ ListaDeExercicios.Exercicio15/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ListaDeExercicios.Exercicio15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            for (numero = 1; numero < 101; numero++)
+            {
+
+                if (numero % 3 == 0 && numero % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (numero % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (numero % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                    Console.WriteLine(numero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
