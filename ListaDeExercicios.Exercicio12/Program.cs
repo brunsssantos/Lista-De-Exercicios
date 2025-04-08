62fe2cda23ef80352e08b7b09874a2343f4b704d
Bruna Santos <brunasan7os@gmail.com>
2025-04-07 22:41:28 -0300

    Exercício 12

---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+            for (int i = 101; i <=199; i+= 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
