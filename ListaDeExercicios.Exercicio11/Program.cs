ac2420351844cb807d447c402ae0c2d82fd21342
Bruna Santos <brunasan7os@gmail.com>
2025-04-07 22:36:41 -0300

    Exercício 11

---
+++ ListaDeExercicios.Exercicio11/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ListaDeExercicios.Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+                Console.WriteLine("Informe o número: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("O número é par");
+                }
+                else
+                    Console.WriteLine("O número é ímpar");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
